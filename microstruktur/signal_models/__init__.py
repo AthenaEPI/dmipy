b0d03f32366cdf232444f778488d1afe5955608c
Demian Wassermann <demian.wassermann@inria.fr>
2017-03-22 15:21:43 +0100

    Added Bar-Shir paper replication

--- microstruktur/signal_models/__init__.py
+++ microstruktur/signal_models/__init__.py
@@ -1,17 +1,12 @@
 """Several tools for Diffusion MRI sequence simulation."""
 from __future__ import division
 from .constants import CONSTANTS
-from . import multiple_correlation_function
-from . import callaghan
 from .free_diffusion import free_diffusion_attenuation
-from . import gaussian_phase
-from . import narrow_pulse
-from .convert_units import (
+from ..convert_units import (
     unit_conversion_factor,
     unit_conversion_factor_from_SI, unit_conversion_factor_to_SI
 )
 from .gradient_conversions import *
-#from . import three_dimensional_models
 
 
 def max_diffusion_time(
