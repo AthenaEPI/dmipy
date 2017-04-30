d2e12abc147ee04e6bad30beb033a300aa76bb68
Rutger Fick <rutger.fick@inria.fr>
2017-04-30 10:23:47 +0200

    adapted stick test to be compatible with new class microstructure functions

--- microstruktur/signal_models/tests/test_stick.py
+++ microstruktur/signal_models/tests/test_stick.py
@@ -1,39 +1,32 @@
-from numpy.testing import (assert_almost_equal,
-                           assert_array_almost_equal,
-                           assert_equal,
-                           run_module_suite)
+from numpy.testing import assert_almost_equal
 import numpy as np
-from microstruktur.signal_models.three_dimensional_models import I1_stick
+from microstruktur.signal_models import three_dimensional_models
 from microstruktur.signal_models.utils import perpendicular_vector
+from dipy.core.geometry import sphere2cart
+DIFFUSIVITY_SCALING = 1e-3
 
 
 def test_orienting_stick():
     # test for orienting the axis of the Stick along mu
     # first test to see if Estick equals Gaussian with lambda_par along mu
-    random_n_mu_vector = np.random.rand(3)
-    random_n_mu_vector /= np.linalg.norm(random_n_mu_vector)
-    random_bval = np.random.rand() * 1000.
-    random_lambda_par = np.random.rand() * 3e-3
-    E_stick = I1_stick(random_bval, random_n_mu_vector,
-                       random_n_mu_vector, random_lambda_par)
-    E_check = np.exp(-random_bval * random_lambda_par)
+    random_n_mu_vector = np.random.rand(2) * np.pi
+    x, y, z = sphere2cart(1, random_n_mu_vector[0], random_n_mu_vector[1])
+    n = np.array([x, y, z])
+    random_bval = np.r_[np.random.rand() * 1000.]
+    random_lambda_par = np.random.rand() * 3
+    shell_index = np.r_[1]
+
+    # initialize model
+    stick = three_dimensional_models.I1Stick(mu=random_n_mu_vector,
+                                             lambda_par=random_lambda_par)
+
+    # test if parallel direction attenuation as a Gaussian
+    E_stick = stick(bvals=random_bval, n=n, shell_indices=shell_index)
+    E_check = np.exp(-random_bval * (random_lambda_par * DIFFUSIVITY_SCALING))
     assert_almost_equal(E_stick, E_check)
 
-    # second test to see if perpendicular vector is unity
-    random_n_mu_vector_perp = perpendicular_vector(random_n_mu_vector)
-    E_stick_perp = I1_stick(random_bval, random_n_mu_vector_perp,
-                            random_n_mu_vector, random_lambda_par)
-    assert_equal(E_stick_perp, 1.)
-
-
-def test_array_stick(n_length=10):
-    # test to see if n can also be given as an array.
-    random_mu_vector = np.random.rand(3)
-    random_mu_vector /= np.linalg.norm(random_mu_vector)
-    random_n_vector = np.random.rand(n_length, 3)
-    random_n_vector /= np.linalg.norm(random_n_vector, axis=0)
-    random_bval = np.random.rand() * 1000.
-    random_lambda_par = np.random.rand() * 3e-3
-    E_stick = I1_stick(random_bval, random_n_vector,
-                       random_mu_vector, random_lambda_par)
-    assert_equal(E_stick.shape[0], n_length)
+    # test if perpendicular direction does not attenuate
+    n_perp = perpendicular_vector(n)
+    E_stick_perp = stick(bvals=random_bval, n=n_perp,
+                         shell_indices=shell_index)
+    assert_almost_equal(E_stick_perp, 1.)
