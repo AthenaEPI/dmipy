5fd9d9058ae19b9b738dea33090f2bcf31e01737
Rutger Fick <rutger.fick@inria.fr>
2017-10-19 01:35:32 +0200

    added dispersed soderman tests

--- microstruktur/signal_models/tests/test_cylinder_models.py
+++ microstruktur/signal_models/tests/test_cylinder_models.py
@@ -1,8 +1,10 @@
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_almost_equal, assert_equal
 import numpy as np
 from microstruktur.signal_models import three_dimensional_models
 from microstruktur.signal_models import dispersed_models
 from microstruktur.signal_models.gradient_conversions import b_from_q
+from dipy.data import get_sphere
+sphere = get_sphere().subdivide()
 DIFFUSIVITY_SCALING = 1e-9
 
 
@@ -37,3 +39,47 @@ def test_RTAP_to_diameter_soderman_callaghan(samples=10000):
 
     assert_almost_equal(diameter_soderman, diameter, 7)
     assert_almost_equal(diameter_callaghan, diameter, 7)
+
+
+def test_watson_dispersed_soderman_kappa0(
+    lambda_par=1.7, diameter=8e-6, bvalue=1e9, mu=[0, 0], kappa=0
+):
+
+    # testing uniformly dispersed watson zeppelin.
+    n = sphere.vertices
+    shell_indices = np.ones(len(n))
+    bvals = np.tile(bvalue, len(n))
+    delta = np.tile(1e-2, len(bvals))
+    Delta = np.tile(3e-2, len(bvals))
+
+    watson_soderman = dispersed_models.SD3I2WatsonDispersedSodermanCylinder(
+        mu=mu, kappa=kappa, lambda_par=lambda_par, diameter=diameter)
+    E_watson_soderman = watson_soderman(bvals=bvals, n=n,
+                                        shell_indices=shell_indices,
+                                        delta=delta, Delta=Delta)
+    E_unique_watson_soderman = np.unique(E_watson_soderman)
+    # All values are the same:
+    assert_equal(len(E_unique_watson_soderman), 1)
+
+
+def test_bingham_dispersed_soderman_kappa0(
+    lambda_par=1.7, diameter=8e-6, bvalue=1e9, mu=[0, 0], kappa=0, beta=0,
+    psi=0
+):
+    # testing uniformly dispersed bingham zeppelin.
+    n = sphere.vertices
+    shell_indices = np.ones(len(n))
+    bvals = np.tile(bvalue, len(n))
+    delta = np.tile(1e-2, len(bvals))
+    Delta = np.tile(3e-2, len(bvals))
+
+    bingham_soderman = dispersed_models.SD2I2BinghamDispersedSodermanCylinder(
+        mu=mu, kappa=kappa, beta=beta, psi=psi, lambda_par=lambda_par,
+        diameter=diameter
+    )
+    E_bingham_soderman = bingham_soderman(bvals=bvals, n=n,
+                                          shell_indices=shell_indices,
+                                          delta=delta, Delta=Delta)
+    E_unique_bingham_soderman = np.unique(E_bingham_soderman)
+    # All values are the same:
+    assert_equal(len(E_unique_bingham_soderman), 1)
