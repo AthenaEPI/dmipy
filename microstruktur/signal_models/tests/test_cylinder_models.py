3f270ac009852aee3c50a761551b9cb1ed0ffe1a
Rutger Fick <rutger.fick@inria.fr>
2017-10-20 00:31:02 +0200

    added tests for dispersed cylinder models.

--- microstruktur/signal_models/tests/test_cylinder_models.py
+++ microstruktur/signal_models/tests/test_cylinder_models.py
@@ -83,3 +83,95 @@ def test_bingham_dispersed_soderman_kappa0(
     E_unique_bingham_soderman = np.unique(E_bingham_soderman)
     # All values are the same:
     assert_equal(len(E_unique_bingham_soderman), 1)
+
+
+def test_watson_dispersed_callaghan_kappa0(
+    lambda_par=1.7, diameter=8e-6, bvalue=1e9, mu=[0, 0], kappa=0
+):
+
+    # testing uniformly dispersed watson zeppelin.
+    n = sphere.vertices
+    shell_indices = np.ones(len(n))
+    bvals = np.tile(bvalue, len(n))
+    delta = np.tile(1e-2, len(bvals))
+    Delta = np.tile(3e-2, len(bvals))
+
+    watson_callaghan = dispersed_models.SD3I3WatsonDispersedCallaghanCylinder(
+        mu=mu, kappa=kappa, lambda_par=lambda_par, diameter=diameter)
+    E_watson_callaghan = watson_callaghan(bvals=bvals, n=n,
+                                          shell_indices=shell_indices,
+                                          delta=delta, Delta=Delta)
+    E_unique_watson_callaghan = np.unique(E_watson_callaghan)
+    # All values are the same:
+    assert_equal(len(E_unique_watson_callaghan), 1)
+
+
+def test_bingham_dispersed_callaghan_kappa0(
+    lambda_par=1.7, diameter=8e-6, bvalue=1e9, mu=[0, 0], kappa=0, beta=0,
+    psi=0
+):
+    # testing uniformly dispersed bingham zeppelin.
+    n = sphere.vertices
+    shell_indices = np.ones(len(n))
+    bvals = np.tile(bvalue, len(n))
+    delta = np.tile(1e-2, len(bvals))
+    Delta = np.tile(3e-2, len(bvals))
+
+    bingham_callaghan = (
+        dispersed_models.SD2I3BinghamDispersedCallaghanCylinder(
+            mu=mu, kappa=kappa, beta=beta, psi=psi, lambda_par=lambda_par,
+            diameter=diameter)
+    )
+    E_bingham_callaghan = bingham_callaghan(bvals=bvals, n=n,
+                                            shell_indices=shell_indices,
+                                            delta=delta, Delta=Delta)
+    E_unique_bingham_callaghan = np.unique(E_bingham_callaghan)
+    # All values are the same:
+    assert_equal(len(E_unique_bingham_callaghan), 1)
+
+
+def test_watson_dispersed_gaussian_phase_kappa0(
+    lambda_par=1.7, diameter=8e-6, bvalue=1e9, mu=[0, 0], kappa=0
+):
+
+    # testing uniformly dispersed watson zeppelin.
+    n = sphere.vertices
+    shell_indices = np.ones(len(n))
+    bvals = np.tile(bvalue, len(n))
+    delta = np.tile(1e-2, len(bvals))
+    Delta = np.tile(3e-2, len(bvals))
+
+    watson_gaussian_phase = (
+        dispersed_models.SD3I4WatsonDispersedGaussianPhaseCylinder(
+            mu=mu, kappa=kappa, lambda_par=lambda_par, diameter=diameter)
+    )
+    E_watson_gaussian_phase = watson_gaussian_phase(
+        bvals=bvals, n=n, shell_indices=shell_indices, delta=delta,
+        Delta=Delta)
+    E_unique_watson_gaussian_phase = np.unique(E_watson_gaussian_phase)
+    # All values are the same:
+    assert_equal(len(E_unique_watson_gaussian_phase), 1)
+
+
+def test_bingham_dispersed_gaussian_phase_kappa0(
+    lambda_par=1.7, diameter=8e-6, bvalue=1e9, mu=[0, 0], kappa=0, beta=0,
+    psi=0
+):
+    # testing uniformly dispersed bingham zeppelin.
+    n = sphere.vertices
+    shell_indices = np.ones(len(n))
+    bvals = np.tile(bvalue, len(n))
+    delta = np.tile(1e-2, len(bvals))
+    Delta = np.tile(3e-2, len(bvals))
+
+    bingham_gaussian_phase = (
+        dispersed_models.SD2I4BinghamDispersedGaussianPhaseCylinder(
+            mu=mu, kappa=kappa, beta=beta, psi=psi, lambda_par=lambda_par,
+            diameter=diameter)
+    )
+    E_bingham_gaussian_phase = bingham_gaussian_phase(
+        bvals=bvals, n=n, shell_indices=shell_indices,
+        delta=delta, Delta=Delta)
+    E_unique_bingham_gaussian_phase = np.unique(E_bingham_gaussian_phase)
+    # All values are the same:
+    assert_equal(len(E_unique_bingham_gaussian_phase), 1)
