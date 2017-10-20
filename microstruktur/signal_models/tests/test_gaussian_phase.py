44ebe2e28095e6da5a1b2d4a7dbd085edf9ae0aa
Rutger Fick <rutger.fick@inria.fr>
2017-10-21 01:02:46 +0200

    added gaussianphase test for unrestricted diffusion

--- microstruktur/signal_models/tests/test_gaussian_phase.py
+++ microstruktur/signal_models/tests/test_gaussian_phase.py
@@ -2,16 +2,34 @@ from numpy.testing import assert_almost_equal, assert_equal
 import numpy as np
 from microstruktur.signal_models import three_dimensional_models
 from microstruktur.signal_models import dispersed_models
+from microstruktur.signal_models.gradient_conversions import b_from_q
 from dipy.data import get_sphere
 sphere = get_sphere().subdivide()
 DIFFUSIVITY_SCALING = 1e-9
 
 
-def test_gaussian_phase_profile_narrow_pulse_not_restricted():
+def test_gaussian_phase_profile_narrow_pulse_not_restricted(samples=100):
     # the Gaussian Phase model approaches the following equation
     # when delta << tau according to Eq. (13) in VanGelderen et al:
     # np.exp(-2 * (gamma * G * delta * lambda_perp) ** 2)
-    return None
+    mu = [0, 0]
+    lambda_par = .1
+    diameter = 1e-4
+    diffusion_perpendicular = 1.7e-09
+
+    delta = np.tile(1e-3, samples)  # delta towards zero
+    Delta = np.tile(1e-3, samples)  # Delta towards infinity
+    qvals_perp = np.linspace(0, 3e5, samples)
+    bvals_perp = b_from_q(qvals_perp, delta, Delta)
+    n_perp = np.tile(np.r_[1., 0., 0.], (samples, 1))
+
+    vangelderen = (
+        three_dimensional_models.I4CylinderGaussianPhaseApproximation(
+            mu=mu, lambda_par=lambda_par, diameter=diameter)
+    )
+    E_vangelderen = vangelderen(bvals_perp, n_perp, delta=delta, Delta=Delta)
+    E_free_diffusion = np.exp(-bvals_perp * diffusion_perpendicular)
+    assert_equal(np.max(np.abs(E_vangelderen - E_free_diffusion)) < 0.01, True)
 
 
 def test_gaussian_phase_profile_narrow_pulse_restricted():
