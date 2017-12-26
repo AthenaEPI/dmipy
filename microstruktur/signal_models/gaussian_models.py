6ef01647491f344c6eea299272ebcdd97df9a7b4
Rutger Fick <rutger.fick@inria.fr>
2017-12-26 16:23:19 +0100

    removed Dot from gaussian models

--- microstruktur/signal_models/gaussian_models.py
+++ microstruktur/signal_models/gaussian_models.py
@@ -33,47 +33,6 @@ DIFFUSIVITY_SCALING = 1e-9
 A_SCALING = 1e-12
 
 
-class G2Dot(MicrostructureModel):
-    r""" The Dot model [1] - an non-diffusing compartment.
-
-    Parameters
-    ----------
-    no parameters
-
-    References
-    ----------
-    .. [1] Panagiotaki et al.
-           "Compartment models of the diffusion MR signal in brain white
-            matter: a taxonomy and comparison". NeuroImage (2012)
-    """
-
-    _parameter_ranges = {
-    }
-    _parameter_scales = {
-    }
-    spherical_mean = False
-
-    def __init__(self, dummy=None):
-        self.dummy = dummy
-
-    def __call__(self, acquisition_scheme, **kwargs):
-        r'''
-        Parameters
-        ----------
-        acquisition_scheme : acquisition scheme object
-            contains all information on acquisition parameters such as bvalues,
-            gradient directions, etc. Created from acquisition_scheme module.
-
-        Returns
-        -------
-        attenuation : float or array, shape(N),
-            signal attenuation
-        '''
-
-        E_dot = np.ones(acquisition_scheme.number_of_measurements)
-        return E_dot
-
-
 class G3Ball(MicrostructureModel):
     r""" The Ball model [1] - an isotropic Tensor with one diffusivity.
 
@@ -335,7 +294,7 @@ class G5RestrictedZeppelin(MicrostructureModel):
         simple_acq_scheme_rh.Delta = np.full(samples, Delta)
 
         E_kernel_sf = self(simple_acq_scheme_rh, mu=[0., 0.],
-            lambda_par=lambda_par, lambda_inf=lambda_inf, A=A)
+                           lambda_par=lambda_par, lambda_inf=lambda_inf, A=A)
         rh = np.dot(inverse_rh_matrix_kernel[rh_order], E_kernel_sf)
         return rh
 
