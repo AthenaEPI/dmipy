aec2d115de081dd7a0ccc4b2b4eae4ad0d4508c5
Rutger Fick <fick.rutger@gmail.com>
2018-01-29 21:57:07 +0100

    relative links

--- mipy/core/acquisition_scheme.py
+++ mipy/core/acquisition_scheme.py
@@ -1,7 +1,7 @@
 import numpy as np
-from mipy.core.gradient_conversions import (
+from .gradient_conversions import (
     g_from_b, q_from_b, b_from_q, g_from_q, b_from_g, q_from_g)
-from mipy.utils import utils
+from ..utils import utils
 from dipy.reconst.shm import real_sym_sh_mrtrix
 from scipy.cluster.hierarchy import fcluster, linkage
 from dipy.core.gradients import gradient_table, GradientTable
