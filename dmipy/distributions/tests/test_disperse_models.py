7dfed53c3f4e52b30b50b6398ceac35890cdf7f9
Rutger Fick <rutger.fick@inria.fr>
2018-02-05 18:26:51 +0100

    added test for distributed models

--- dmipy/distributions/tests/test_disperse_models.py
+++ dmipy/distributions/tests/test_disperse_models.py
@@ -1,4 +1,4 @@
-from dmipy.signal_models import cylinder_models, gaussian_models
+from dmipy.signal_models import cylinder_models, gaussian_models, sphere_models
 from dmipy.distributions import distribute_models
 from dmipy.data.saved_acquisition_schemes import wu_minn_hcp_acquisition_scheme
 import numpy as np
@@ -30,5 +30,32 @@ def test_all_models_dispersable():
             for param, card in dist_mod.parameter_cardinality.items():
                 params[param] = np.random.rand(
                     card) * dist_mod.parameter_scales[param]
+            assert_equal(isinstance(
+                dist_mod(scheme, **params), np.ndarray), True)
+
+
+def test_all_models_distributable():
+    scheme = wu_minn_hcp_acquisition_scheme()
+
+    distributable_models = [
+        cylinder_models.C2CylinderSodermanApproximation,
+        cylinder_models.C3CylinderCallaghanApproximation,
+        cylinder_models.C4CylinderGaussianPhaseApproximation,
+        sphere_models.S2SphereSodermanApproximation
+    ]
+
+    spatial_distributions = [
+        distribute_models.DD1GammaDistributed
+    ]
+
+    for model in distributable_models:
+        for distribution in spatial_distributions:
+            mod = model()
+            dist_mod = distribution([mod])
+            params = {}
+            for param, card in dist_mod.parameter_cardinality.items():
+                params[param] = np.random.rand(
+                    card) * dist_mod.parameter_scales[param]
+            print dist_mod(scheme, **params)
             assert_equal(isinstance(
                 dist_mod(scheme, **params), np.ndarray), True)
