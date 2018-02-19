8e3635badbf4ecfbfab8dab5562dae28486920b2
Rutger Fick <fick.rutger@gmail.com>
2018-02-19 11:46:57 +0100

    added test for duval 3d data

--- dmipy/data/tests/test_saved_data.py
+++ dmipy/data/tests/test_saved_data.py
@@ -1,5 +1,9 @@
-from dmipy.data.saved_data import duval_cat_spinal_cord_2d
+from dmipy.data.saved_data import (
+    duval_cat_spinal_cord_2d,
+    duval_cat_spinal_cord_3d
+)
 
 
-def test_loading_saved_duval_data_2d():
+def test_loading_saved_duval_data_2dand3d():
     scheme, data = duval_cat_spinal_cord_2d()
+    scheme, data = duval_cat_spinal_cord_3d()
