4e36374482c23c2312c3ade88f2f085fedbe8033
Rutger Fick <rutger.fick@inria.fr>
2018-02-24 00:36:20 +0100

    added verdict loading test

--- dmipy/data/tests/test_saved_data.py
+++ dmipy/data/tests/test_saved_data.py
@@ -1,7 +1,8 @@
 from dmipy.data.saved_data import (
     duval_cat_spinal_cord_2d,
     duval_cat_spinal_cord_3d,
-    isbi2015_white_matter_challenge
+    isbi2015_white_matter_challenge,
+    panagiotaki_verdict
 )
 
 
@@ -9,3 +10,4 @@ def test_loading_saved_duval_data_2dand3d():
     scheme, data = duval_cat_spinal_cord_2d()
     scheme, data = duval_cat_spinal_cord_3d()
     scheme, data1, data2 = isbi2015_white_matter_challenge()
+    scheme, data = panagiotaki_verdict()
