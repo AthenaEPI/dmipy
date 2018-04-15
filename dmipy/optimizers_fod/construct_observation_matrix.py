090ed9629602a2e7a356bfe212bbe78c8e55eeac
Rutger Fick <rutger.fick@inria.fr>
2018-04-15 21:38:51 +0200

    added multi-shell sh-matrix construction

---
+++ dmipy/optimizers_fod/construct_observation_matrix.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+
+def construct_model_based_A_matrix(acquisition_scheme, model, lmax):
+    """Constructs the multi-shell observation matrix from spherical_harmonics
+    to DWIs. Follows the notation of Eq. (2) in [1]_.
+
+    The dmipy acquisition_scheme object contains all the information on which
+    DWIs belong to which acquisition shells, what are the maximum spherical
+    harmonics order used for each shell, and the observation matrix that maps
+    the DWIs of each shell to a spherical harmonics representation.
+
+    The dmipy model must be have all parameters fixed to be able to generate
+    the rotational harmonics of each shell.
+
+    Parameters
+    ----------
+    acquisition_scheme : DmipyAcquisitionScheme instance,
+            An acquisition scheme that has been instantiated using dmipy.
+    model: dmipy signal model,
+        dmipy model with all parameters fixed.
+    lmax: even positive integer,
+        even maximum spherical harmonics order of the to-be-estimated FOD.
+
+    Returns
+    -------
+    Ams: array of size (N_DWIs, N_sh_coef),
+        observation matrix to map spherical harmonics to DWIs.
+
+    References
+    ----------
+    .. [1] Jeurissen, Ben, et al. "Multi-tissue constrained spherical
+        deconvolution for improved analysis of multi-shell diffusion MRI data."
+        NeuroImage 103 (2014): 411-426.
+    """
+    Ncoef = int((lmax + 2) * (lmax + 1) // 2)
+    Ams = np.zeros([acquisition_scheme.number_of_measurements, Ncoef])
+    Ams[acquisition_scheme.b0_mask, 0] = 2 * np.sqrt(np.pi)
+
+    model_rh = model.rotational_harmonics_representation(acquisition_scheme)
+
+    sh_eigenvalues = np.zeros([len(model_rh), Ncoef])
+
+    # prepare the rotational harmonics of the kernel
+    counter = 0
+    for n_ in range(0, lmax + 1, 2):
+        coef_in_order = 2 * n_ + 1
+        sh_eigenvalues[:, counter: counter + coef_in_order] = (
+            np.sqrt((4 * np.pi) / (2 * n_ + 1)) *   # sh eigenvalues
+            model_rh[:, n_ // 2: n_ // 2 + 1])
+
+        counter += coef_in_order
+
+    # construct the multi-shell observation matrix.
+    for i, shell_index in enumerate(acquisition_scheme.unique_dwi_indices):
+        shell_mask = acquisition_scheme.shell_indices == shell_index
+        shell_sh_matrix = acquisition_scheme.shell_sh_matrices[shell_index]
+        if Ncoef < shell_sh_matrix.shape[1]:
+            Ams[shell_mask, :Ncoef] = np.dot(
+                shell_sh_matrix[:, :Ncoef], np.diag(sh_eigenvalues[i]))
+        else:
+            Ams[shell_mask, :shell_sh_matrix.shape[1]] = np.dot(
+                shell_sh_matrix,
+                np.diag(sh_eigenvalues[i, :shell_sh_matrix.shape[1]]))
+    return Ams
---
+++ dmipy/optimizers_fod/tests/test_constructions_observation_matrix.py
@@ -0,0 +1,30 @@
+from dmipy.signal_models import cylinder_models
+from dmipy.distributions import distribute_models, distributions
+import numpy as np
+from dmipy.data.saved_acquisition_schemes import (
+    wu_minn_hcp_acquisition_scheme)
+from dmipy.optimizers_fod.construct_observation_matrix import (
+    construct_model_based_A_matrix)
+
+
+scheme = wu_minn_hcp_acquisition_scheme()
+
+
+def test_construction_observation_matrix(
+        odi=0.15, mu=[0., 0.], lambda_par=1.7e-9, lmax=8):
+    stick = cylinder_models.C1Stick(lambda_par=lambda_par)
+    watsonstick = distribute_models.SD1WatsonDistributed(
+        [stick])
+
+    params = {'SD1Watson_1_odi': odi,
+              'SD1Watson_1_mu': mu,
+              'C1Stick_1_lambda_par': lambda_par}
+
+    data = watsonstick(scheme, **params)
+    watson = distributions.SD1Watson(mu=mu, odi=odi)
+    sh_watson = watson.spherical_harmonics_representation(lmax)
+
+    A = construct_model_based_A_matrix(scheme, stick, lmax)
+
+    data_approximated = A.dot(sh_watson)
+    np.testing.assert_array_almost_equal(data_approximated, data, 4)
