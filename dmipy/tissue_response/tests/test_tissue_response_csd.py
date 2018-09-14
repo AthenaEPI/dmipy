f306959a105ebe19c4522d0c70ab70af22726d5a
Rutger Fick <fick.rutger@gmail.com>
2018-09-14 10:43:34 +0200

    correct S0 test for csd

--- dmipy/tissue_response/tests/test_tissue_response_csd.py
+++ dmipy/tissue_response/tests/test_tissue_response_csd.py
@@ -10,14 +10,14 @@ import numpy as np
 scheme = wu_minn_hcp_acquisition_scheme()
 
 
-def test_fit_S0_response():
+def test_fit_S0_response(S0_iso=10., S0_aniso=1.):
     ball = G1Ball(lambda_iso=3e-9)
-    data_iso = 10. * ball(scheme)
+    data_iso = S0_iso * ball(scheme)
     iso_model = IsotropicTissueResponseModel(scheme, np.atleast_2d(data_iso))
 
     zeppelin = G2Zeppelin(
         lambda_par=2.2e-9, lambda_perp=1e-9, mu=[np.pi / 2, np.pi / 2])
-    data_aniso = zeppelin(scheme)
+    data_aniso = S0_aniso * zeppelin(scheme)
     aniso_model = AnisotropicTissueResponseModel(
         scheme, np.atleast_2d(data_aniso))
 
@@ -28,9 +28,10 @@ def test_fit_S0_response():
     csd_fit_no_S0 = mtcsd.fit(scheme, data_to_fit, fit_S0_response=False)
     csd_fit_S0 = mtcsd.fit(scheme, data_to_fit, fit_S0_response=True)
     np.testing.assert_almost_equal(
-        0.3, csd_fit_S0.fitted_parameters['partial_volume_0'], 4)
+        0.3, csd_fit_S0.fitted_parameters['partial_volume_0'], 3)
     np.testing.assert_almost_equal(
-        0.7, csd_fit_S0.fitted_parameters['partial_volume_1'], 4)
+        0.7, csd_fit_S0.fitted_parameters['partial_volume_1'], 3)
 
-    # test iso volume fraction underestimated
-    np.testing.assert_(csd_fit_no_S0 < 0.3)
+    # test iso volume fraction overestimated without S0 response
+    np.testing.assert_(
+        csd_fit_no_S0.fitted_parameters['partial_volume_0'] > 0.3)
