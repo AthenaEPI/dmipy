f5edff83d3dd0e3af6ca8c8f3241ae4fdfeb9a1e
Rutger Fick <rutger.fick@inria.fr>
2018-09-15 18:05:15 +0200

    added tournier07 test

--- dmipy/tissue_response/tests/test_tournier_wm_response.py
+++ dmipy/tissue_response/tests/test_tournier_wm_response.py
@@ -5,13 +5,14 @@ from dmipy.data.saved_acquisition_schemes import (
 from dmipy.signal_models.tissue_response_models import (
     AnisotropicTissueResponseModel)
 from dmipy.tissue_response.white_matter_response import (
+    white_matter_response_tournier07,
     white_matter_response_tournier13)
 import numpy as np
 from numpy.testing import assert_array_almost_equal, assert_raises
 scheme = wu_minn_hcp_acquisition_scheme()
 
 
-def test_tournier13_picks_single_peak():
+def _make_single_and_crossing():
     zeppelin = G2Zeppelin(
         lambda_par=1.7e-9, lambda_perp=1e-9, mu=[0., 0.])
     data_aniso = zeppelin(scheme)
@@ -35,6 +36,11 @@ def test_tournier13_picks_single_peak():
 
     data_cross = np.array(
         [data_watson_par, data_watson_par + data_watson_perp])
+    return data_cross
+
+
+def test_tournier13_picks_single_peak():
+    data_cross = _make_single_and_crossing()
 
     assert_raises(ValueError,
                   white_matter_response_tournier13,
@@ -45,5 +51,14 @@ def test_tournier13_picks_single_peak():
         N_candidate_voxels=1)
     assert_array_almost_equal(
         wm(scheme, mu=[0., 0.]),
-        watson_mod(
-            scheme, **watson_params_par), 4)
+        data_cross[0], 4)
+
+
+def test_tournier07_picks_single_peak():
+    data_cross = _make_single_and_crossing()
+    wm, _ = white_matter_response_tournier07(
+        scheme, data_cross, peak_ratio_setting='mrtrix',
+        N_candidate_voxels=1)
+    assert_array_almost_equal(
+        wm(scheme, mu=[0., 0.]),
+        data_cross[0], 4)
